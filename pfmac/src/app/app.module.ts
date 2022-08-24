3803129bfa45dde26893025dd9154865ce7758da
jmackern <111448535+jmackern@users.noreply.github.com>
2022-08-23 22:51:00 -0300

    yendo

--- pfmac/src/app/app.module.ts
+++ pfmac/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoapComponent
   ],
   imports: [
     BrowserModule,
